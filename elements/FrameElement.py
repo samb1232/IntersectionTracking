c705e75c329c64131444b085abbe369103ed735b
samb1232 <arterm212@gmail.com>
2024-05-21 19:36:03 +0500

    added types to Elements

--- elements/FrameElement.py
+++ elements/FrameElement.py
@@ -24,26 +24,26 @@ class FrameElement:
             id_list: list | None = None,
             buffer_tracks: dict | None = None,
     ) -> None:
-        self.source = source  # Путь к видео или номер камеры с которой берем поток
-        self.frame = frame  # Кадр bgr формата 
-        self.timestamp = timestamp  # Значение времени с начала потока (в секундах)
-        self.frame_num = frame_num  # Нормер кадра с потока
-        self.roads_info = roads_info  # Словарь с координатми дорог, примыкающих к участку кругового движения
-        self.frame_result = frame_result  # Итоговый обработанный кадр
-        self.timestamp_date = time.time()  # Время в момент обработки кадра unix формат (в секундах)
+        self.source: str = source  # Путь к видео или номер камеры с которой берем поток
+        self.frame: np.ndarray = frame  # Кадр bgr формата
+        self.timestamp: float = timestamp  # Значение времени с начала потока (в секундах)
+        self.frame_num: float = frame_num  # Нормер кадра с потока
+        self.roads_info: dict = roads_info  # Словарь с координатми дорог, примыкающих к участку кругового движения
+        self.frame_result: np.ndarray | None = frame_result  # Итоговый обработанный кадр
+        self.timestamp_date: float = time.time()  # Время в момент обработки кадра unix формат (в секундах)
 
         # Результаты на выходе с YOLO:
-        self.detected_conf = detected_conf  # Список уверенностей задетектированных объектов
-        self.detected_cls = detected_cls  # Список классов задетектированных объектов
-        self.detected_xyxy = detected_xyxy  # Список списков с координатами xyxy боксов
+        self.detected_conf: list | None = detected_conf  # Список уверенностей задетектированных объектов
+        self.detected_cls: list | None = detected_cls  # Список классов задетектированных объектов
+        self.detected_xyxy: list[list] | None = detected_xyxy  # Список списков с координатами xyxy боксов
 
         # Результаты корректировки трекинг алгоритмом:
-        self.tracked_conf = tracked_conf  # Список уверенностей задетектированных объектов
-        self.tracked_cls = tracked_cls  # Список классов задетектированных объектов
-        self.tracked_xyxy = tracked_xyxy  # Список списков с координатами xyxy боксов    
-        self.id_list = id_list  # Список обнаруженных id трекуемых объектов
+        self.tracked_conf: list | None = tracked_conf  # Список уверенностей задетектированных объектов
+        self.tracked_cls: list | None = tracked_cls  # Список классов задетектированных объектов
+        self.tracked_xyxy: list[list] | None  = tracked_xyxy  # Список списков с координатами xyxy боксов
+        self.id_list: list | None = id_list  # Список обнаруженных id трекуемых объектов
 
         # Постобработка кадра:
-        self.buffer_tracks = buffer_tracks  # Буфер актуальных треков за выбранное время анализа
-        self.info = {}  # Словарь с результирующей статистикой (загруженность дорог + число машин)
-        self.send_info_of_frame_to_db = False  # Флаг того, будет ли с это кадра инфа отправлена в бд
+        self.buffer_tracks: dict | None = buffer_tracks  # Буфер актуальных треков за выбранное время анализа
+        self.info: dict = {}  # Словарь с результирующей статистикой (загруженность дорог + число машин)
+        self.send_info_of_frame_to_db: bool = False  # Флаг того, будет ли с это кадра инфа отправлена в бд
--- elements/TrackElement.py
+++ elements/TrackElement.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class TrackElement:
     # Класс, содержаций информацию о конкретном треке машины
     def __init__(
@@ -6,12 +9,12 @@ class TrackElement:
         timestamp_first: float,
         start_road: int | None = None,
     ) -> None:
-        self.id = id  # Номер этого трека
-        self.timestamp_first = timestamp_first  # Таймстемп инициализации (в сек)
-        self.timestamp_last = timestamp_first  # Таймстемп последнего обнаружения (в сек)
-        self.start_road = start_road  # Номер дороги, с которой приехал
-        self.end_road = None  # Номер дороги, на которую уехал
-        self.timestamp_init_road = timestamp_first  # Таймстемп инициализации номера дороги (в сек)
+        self.id: int = id  # Номер этого трека
+        self.timestamp_first: float = timestamp_first  # Таймстемп инициализации (в сек)
+        self.timestamp_last: float = timestamp_first  # Таймстемп последнего обнаружения (в сек)
+        self.start_road: int | None = start_road  # Номер дороги, с которой приехал
+        self.end_road: int | None = None  # Номер дороги, на которую уехал
+        self.timestamp_init_road: float = timestamp_first  # Таймстемп инициализации номера дороги (в сек)
         # ps: если дорога не будет определена, то значение останется равным первому появлению
 
     def update(self, timestamp):
